bd4af610efe68e94811a4413923cfcbf2e07b95d
fleddd <fedkiv20172@gmail.com>
2025-12-15 19:57:56 +0200

    Fixed tests

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import { render, screen, within } from "@testing-library/react";
+import App from "./App";
+
+describe("My dream team landing", () => {
+  test("renders header title", () => {
+    render(<App />);
+    expect(
+      screen.getByRole("heading", { level: 1, name: /My dream team/i })
+    ).toBeInTheDocument();
+  });
+});
